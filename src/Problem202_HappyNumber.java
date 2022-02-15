2740f211eaf023013c5c72ec46c7e7ebc2e9f4ef
Adarsha <adarshacr001@gmail.com>
2022-02-15 19:46:00 +0530

    Solved problem 202

---
+++ src/Problem202_HappyNumber.java
@@ -0,0 +1,20 @@
+public class Problem202_HappyNumber {
+    public boolean isHappy(int n) {
+        int max=6;
+        while(n!=1&&max>0){
+            int sum=0;
+            while(n!=0){
+                int reminder=n%10;
+                n=n/10;
+                sum+=(reminder*reminder);
+            }
+            n=sum;
+            max--;
+        }
+        if(n==1){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
