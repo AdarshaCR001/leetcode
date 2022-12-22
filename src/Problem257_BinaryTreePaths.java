3acf116f0615cd7a34d39760e3f3c75c873462f3
Adarsha <adarshacr001@gmail.com>
2022-12-22 20:34:00 +0530

    Solved problem 257

---
+++ src/Problem257_BinaryTreePaths.java
@@ -0,0 +1,32 @@
+import helper.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Problem257_BinaryTreePaths {
+    private List<String> result=new ArrayList<>();
+    public List<String> binaryTreePaths(TreeNode root) {
+        if(root!=null){
+            binaryTreePaths(root, new StringBuilder());
+        }
+        return result;
+    }
+
+    private void binaryTreePaths(TreeNode root, StringBuilder path){
+        if(path.isEmpty()){
+            path.append(root.val);
+        }else{
+            path.append("->"+root.val);
+        }
+        if(root.left==null && root.right==null){
+            result.add(path.toString());
+            return;
+        }
+        if(root.left!=null){
+            binaryTreePaths(root.left, new StringBuilder(path));
+        }
+        if(root.right!=null){
+            binaryTreePaths(root.right, new StringBuilder(path));
+        }
+    }
+}
