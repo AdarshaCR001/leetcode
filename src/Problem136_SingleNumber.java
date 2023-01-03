2851d682add6a5f90f848280b82350c5e1e98601
Adarsha <adarshacr001@gmail.com>
2023-01-03 20:45:00 +0530

    Solved problem 136

---
+++ src/Problem136_SingleNumber.java
@@ -0,0 +1,9 @@
+public class Problem136_SingleNumber {
+    public int singleNumber(int[] nums) {
+        int num=0;
+        for(int i:nums){
+            num=num^i;
+        }
+        return num;
+    }
+}
