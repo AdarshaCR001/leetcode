038d943c6bcab5b21675d725e7baee07d6378d40
Adarsha <adarshacr001@gmail.com>
2023-01-09 19:44:00 +0530

    Solved problem 151

---
+++ src/Problem151_ReverseWordsInAString.java
@@ -0,0 +1,13 @@
+public class Problem151_ReverseWordsInAString {
+    public String reverseWords(String s) {
+        String[] words=s.split(" ");
+        StringBuilder sb=new StringBuilder();
+        for(int i=words.length-1;i>=0;i--){
+            if(!words[i].trim().isEmpty()){
+                sb.append(words[i]);
+                sb.append(" ");
+            }
+        }
+        return sb.toString().trim();
+    }
+}
