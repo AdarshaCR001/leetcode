e1e6a25109a0676739306daf2eb268917b622205
Adarsha <adarshacr001@gmail.com>
2023-02-05 19:45:00 +0530

    Solved problem 1141

---
+++ mysql/Problem1141_UserActivityForThePast30Days.sql
@@ -0,0 +1,4 @@
+# Write your MySQL query statement below
+select a.activity_date as day,count(distinct user_id) as active_users from Activity a
+where a.activity_date>="2019-06-28" and a.activity_date<="2019-07-27"
+group by a.activity_date;
