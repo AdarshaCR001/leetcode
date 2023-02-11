ae0a41bce2c28e6effb98dfad66702a9e8d20e7c
Adarsha <adarshacr001@gmail.com>
2023-02-11 19:45:00 +0530

    Solved problem 1158

---
+++ mysql/Problem1158_MarketAnalysis.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+SELECT u.user_id AS buyer_id, u.join_date,
+count(o.order_date) AS orders_in_2019
+FROM users u
+LEFT JOIN orders o ON o.buyer_id=u.user_id
+AND YEAR(o.order_date) = "2019"
+GROUP BY u.user_id;
