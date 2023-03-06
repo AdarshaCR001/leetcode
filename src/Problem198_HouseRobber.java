0f77ade17c86eb6ed34e1e81a68ac2179a4d0a70
Adarsha <adarshacr001@gmail.com>
2023-03-06 19:44:00 +0530

    Solved problem 198

---
+++ src/Problem198_HouseRobber.java
@@ -0,0 +1,12 @@
+public class Problem198_HouseRobber {
+    public int rob(int[] nums) {
+        int prev1=0;
+        int prev2=0;
+        for(int i=0;i<nums.length;i++){
+            int temp=prev1;
+            prev1=Math.max(prev2+nums[i],prev1);
+            prev2=temp;
+        }
+        return prev1;
+    }
+}
