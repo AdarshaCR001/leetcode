bc891ac6c693ed414fa41d726b4f83dd6a71b861
Adarsha <adarshacr001@gmail.com>
2023-03-11 19:45:00 +0530

    Solved problem 199

---
+++ src/Problem199_BinaryTreeRightSideView.java
@@ -0,0 +1,23 @@
+import helper.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Problem199_BinaryTreeRightSideView {
+    List<Integer> result=new ArrayList<>();
+    public List<Integer> rightSideView(TreeNode root) {
+        rightSideView(root,0);
+        return result;
+    }
+    private void rightSideView(TreeNode root, int pos){
+        if(root==null){
+            return;
+        }
+        if(result.size()==pos){
+            result.add(root.val);
+        }
+        pos++;
+        rightSideView(root.right,pos);
+        rightSideView(root.left,pos);
+    }
+}
