6a4650f86a7f2e3dfc1565a44a4992035fae305a
Adarsha <adarshacr001@gmail.com>
2023-03-13 19:45:00 +0530

    Solved problem 231

---
+++ src/Problem231_PowerOfTwo.java
@@ -0,0 +1,14 @@
+public class Problem231_PowerOfTwo {
+    public boolean isPowerOfTwo(int n) {
+        while (n > 0) {
+            if (n == 1) {
+                return true;
+            }
+            if (n % 2 == 1) {
+                return false;
+            }
+            n = n / 2;
+        }
+        return false;
+    }
+}
