e900966edebc295741b19c8004459e3b12a41894
Adarsha <adarshacr001@gmail.com>
2023-03-14 20:45:00 +0530

    Solved problem 171

---
+++ src/Problem171_ExcelSheetColumnNumber.java
@@ -0,0 +1,12 @@
+public class Problem171_ExcelSheetColumnNumber {
+    public int titleToNumber(String columnTitle) {
+        int count=1;
+        int result=0;
+        for(int i=columnTitle.length()-1;i>=0;i--){
+            int num=columnTitle.charAt(i)-'A'+1;
+            result=result+(num*count);
+            count=count*26;
+        }
+        return result;
+    }
+}
