d1ad13aa5b9e286fad8f0cd00c23b15e127df8ab
Adarsha <adarshacr001@gmail.com>
2023-03-19 19:44:00 +0530

    Solved problem 78

---
+++ src/Problem78_Subsets.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Problem78_Subsets {
+    List<List<Integer>> result=new ArrayList<>();
+    int[] nums;
+    public List<List<Integer>> subsets(int[] nums) {
+        this.nums=nums;
+        result.add(new ArrayList<>());
+        for(int i=0;i<nums.length;i++){
+            combine(new ArrayList<>(), i);
+        }
+        return result;
+    }
+
+    private void combine(List<Integer> array, int pos){
+        array.add(nums[pos]);
+        result.add(array);
+        for(int i=pos+1;i<nums.length;i++){
+            combine(new ArrayList<>(array),i);
+        }
+    }
+}
