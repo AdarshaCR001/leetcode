5de851b84861edacc7ae965a096c91fb12ff2f5d
Adarsha <adarshacr001@gmail.com>
2023-03-23 20:45:00 +0530

    Solved problem 117

---
+++ src/Problem117_PopulatingNextRightPointersInEachNode2.java
@@ -0,0 +1,37 @@
+import helper.Node;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Problem117_PopulatingNextRightPointersInEachNode2 {
+    public Node connect(Node root) {
+        if (root != null) {
+            Queue<Node> newQueue = new LinkedList<>();
+            newQueue.add(root);
+            connectUtil(newQueue);
+        }
+        return root;
+    }
+
+    private void connectUtil(Queue<Node> queue) {
+        if (!queue.isEmpty()) {
+            Queue<Node> newQueue = new LinkedList<>();
+            Node prev = null;
+            while (!queue.isEmpty()) {
+                if (prev == null) {
+                    prev = queue.poll();
+                } else {
+                    prev.next = queue.poll();
+                    prev = prev.next;
+                }
+                if (prev.left != null) {
+                    newQueue.add(prev.left);
+                }
+                if (prev.right != null) {
+                    newQueue.add(prev.right);
+                }
+            }
+            connectUtil(newQueue);
+        }
+    }
+}
