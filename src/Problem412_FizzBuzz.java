0954518c8f5edd80600d098188003672ec741e0f
Adarsha <adarshacr001@gmail.com>
2023-03-26 19:45:00 +0530

    Solved problem 412

---
+++ src/Problem412_FizzBuzz.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Problem412_FizzBuzz {
+    public List<String> fizzBuzz(int n) {
+        List<String> result = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0) {
+                if (i % 5 == 0) {
+                    result.add("FizzBuzz");
+                } else {
+                    result.add("Fizz");
+                }
+            } else if (i % 5 == 0) {
+                result.add("Buzz");
+            } else {
+                result.add(Integer.toString(i));
+            }
+        }
+        return result;
+    }
+}
