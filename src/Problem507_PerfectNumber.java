8942a15340ba8939862a848206325a57bda78ed4
Adarsha <adarshacr001@gmail.com>
2023-04-13 20:58:44 +0530

    Solved Problem 507

---
+++ src/Problem507_PerfectNumber.java
@@ -0,0 +1,17 @@
+public class Problem507_PerfectNumber {
+    public boolean checkPerfectNumber(int num) {
+        if (num % 2 != 0) {
+            return false;
+        }
+        int sum = 0;
+        for (int i = 1; i <= num / 2; i++) {
+            if (num % i == 0) {
+                sum += i;
+            }
+        }
+        if (sum == num) {
+            return true;
+        }
+        return false;
+    }
+}
